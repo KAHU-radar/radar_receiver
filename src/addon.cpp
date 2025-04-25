d8487a11ab922a5fb4cbf6455e160e48e141893f
Egil <egil.moller@freecode.no>
2025-04-25 11:31:38 +0200

    List radar types

--- src/addon.cpp
+++ src/addon.cpp
@@ -1,5 +1,27 @@
 #include <napi.h>
 #include <wx/wx.h>
+#include <wx/string.h>
+#include "radar_info_wrapper.h"
+
+Napi::Array GetRadarTypes(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    Napi::Array result = Napi::Array::New(env, PLUGIN_NAMESPACE::RT_MAX);
+
+    for (size_t i = 0; i < PLUGIN_NAMESPACE::RT_MAX; ++i) {
+        Napi::Object obj = Napi::Object::New(env);
+
+        #define DEFINE_RADAR(t, n, s, l, a, b, c, d) \
+          obj.Set("type", #t); \
+          obj.Set("name", wxString(n).mb_str(wxConvUTF8).data());  \
+          obj.Set("spokes", s); \
+          obj.Set("spokeLength", l);
+        #include "RadarType.h"
+        
+        result[i] = obj;
+    }
+
+    return result;
+}
 
 Napi::String Hello(const Napi::CallbackInfo& info) {
     wxString msg = "Hello from wxWidgets!";
@@ -8,6 +30,8 @@ Napi::String Hello(const Napi::CallbackInfo& info) {
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
     exports.Set("hello", Napi::Function::New(env, Hello));
+    exports.Set("getRadarTypes", Napi::Function::New(env, GetRadarTypes));
+    exports.Set("RadarInfo", RadarInfoWrapper::GetClass(env));
     return exports;
 }
 
--- src/radar_info_wrapper.cpp
+++ src/radar_info_wrapper.cpp
@@ -11,19 +11,6 @@ RadarInfoWrapper::RadarInfoWrapper(const Napi::CallbackInfo& info) : Napi::Objec
         info[0].As<Napi::Number>().Int32Value());
 }
 
-/*
-Napi::Value RadarInfoWrapper::Bar(const Napi::CallbackInfo& info) {
-    Napi::Env env = info.Env();
-    if (info.Length() < 1 || !info[0].IsNumber()) {
-        Napi::TypeError::New(env, "Expected one number argument").ThrowAsJavaScriptException();
-        return env.Null();
-    }
-    int x = info[0].As<Napi::Number>().Int32Value();
-    int result = radar_info_->bar(x);
-    return Napi::Number::New(env, result);
-}
-*/
-
 Napi::Function RadarInfoWrapper::GetClass(Napi::Env env) {
     return DefineClass(env, "RadarInfo", {
       //InstanceMethod("bar", &RadarInfoWrapper::Bar)
