688f31aee4fd26d521f01e23c975e7a208bc089b
Egil <egil.moller@freecode.no>
2025-05-20 09:36:10 +0200

    Test Halo A radar with a radar recording

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 mod = require(".");
 
+var radarTypes = Object.fromEntries(mod.getRadarTypes().map((a, idx) => [a.type, idx]));
+
 ri = new mod.RadarInfo(
-  0,
+  radarTypes.RT_HaloA,
   (angle, bearing, data, range_meters, time) => console.log("SPOKE", angle, bearing, data, range_meters, time),
   (item) => {
     console.log("CONFIG", item);
-    if (item.type.id == "CT_STATE" && item.value.value == 1) {
+    if (item.type.id == "CT_STATE" && item.value.value == 'RADAR_STANDBY') {
       console.log("Start transmitting");
       ri.setProperty('CT_RANGE', 1000);
       ri.setTransmit(true);
@@ -13,6 +15,11 @@ ri = new mod.RadarInfo(
   }
 );
 //console.log("Radar type:", ri.getType());
+
+// ri.setProperty("CT_TRAILS_MOTION", "Relative")
+
+//console.log("CT_TRAILS_MOTION:", ri.getProperty("CT_TRAILS_MOTION"));
+//console.log("CT_TRANSPARENCY:", ri.getProperty("CT_TRANSPARENCY"));
 
 //console.log("Properties:", ri.getProperties());
 
